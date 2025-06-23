07fc68de19e79ebe25c55d635980e401ec9e0f4a
MeguruMacabre <megurumacabre@gmail.com>
2025-06-23 17:34:23 +0300

    New T3 7

--- komarov.maksim/T3/main.cpp
+++ komarov.maksim/T3/main.cpp
@@ -36,8 +36,8 @@ int main(int argc,char* argv[])
         } catch(...) {
             std::cout << "<INVALID COMMAND>\n";
             std::cin.clear();
-            std::string trash;
-            std::getline(std::cin, trash);
+            if (std::cin.peek() != '\n')
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
     }
     return 0;
